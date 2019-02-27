db5c9ea952589587acb218f0dcfe480b9da5d99f
Dan <headhertz88@gmail.com>
2019-02-27 10:45:18 +0000

    Updates

---
+++ precache-manifest.d5fb239951192e7052a08778df5e0a09.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "52fd5bcf459250a1faca",
+    "url": "/waracle-test/static/css/main.9c3c7101.chunk.css"
+  },
+  {
+    "revision": "52fd5bcf459250a1faca",
+    "url": "/waracle-test/static/js/main.52fd5bcf.chunk.js"
+  },
+  {
+    "revision": "9cd910f5c0ce6cf27dcf",
+    "url": "/waracle-test/static/js/runtime~main.9cd910f5.js"
+  },
+  {
+    "revision": "8d3f1bd0e67e2df28bfe",
+    "url": "/waracle-test/static/js/2.8d3f1bd0.chunk.js"
+  },
+  {
+    "revision": "9f944c9b2b44075d39ce3b23f66ace2e",
+    "url": "/waracle-test/index.html"
+  }
+];
